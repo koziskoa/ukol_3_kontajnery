8f5bfd37295ca943b3cad8c2cf06b0925d5dd1ee
Anna Kožíšková <koziskoa@natur.cuni.cz>
2021-12-12 17:18:50 +0100

    změna zápisu kontejnerů

--- code.py
+++ code.py
@@ -17,20 +17,38 @@ def transfer_coor (x=0,y=0):
     wgs2jtsk = Transformer.from_crs(4326,5514, always_xy=True)
     return wgs2jtsk.transform(x,y)
 
-chosen = []
+chosen = {} #[]
+'''{}'''
 counter_public = 0
 counter = 0
 
 containers = open_load("kontejnery.geojson")
 for cont in containers ["features"]:
-        counter += 1
-        if (cont ["properties"] ["PRISTUP"] == "volně"):
-            chosen.append(cont) 
-            counter_public += 1
+    counter += 1
+    if (cont ["properties"] ["PRISTUP"] == "volně"):
+        chosen[cont["properties"]["ID"]] = {\
+        "Street name": cont["properties"]["STATIONNAME"],\
+        "Type": cont["properties"]["PRISTUP"],\
+        "coordinates": [cont["geometry"]["coordinates"][0], 
+                        cont["geometry"]["coordinates"][1]
+                       ]} 
+        counter_public += 1
+    
 print(f"Načteno {counter} kontejnerů na tříděný odpad")
-print(f"{counter_public} kontejnerů je veřejných")
+print(f"        {counter_public} kontejnerů je veřejných\n")
 
+counter_adr = 0
 adress_list = {}
+
+adress = open_load("adresy.geojson")
+for adr in adress ["features"]:
+    adress_list[adr["properties"]["@id"]] = {\
+        "housenumber": adr["properties"]["addr:housenumber"],\
+        "street": adr["properties"]["addr:street"],\
+        "coordinates": transfer_coor(adr["geometry"]["coordinates"][0], adr["geometry"]["coordinates"][1])}
+    if adr["properties"]:
+        counter_adr += 1
+#přehled jak vypadá strktura přepsaných adres
 """
 "id":
 {
@@ -39,16 +57,12 @@ adress_list = {}
     "coordinates": hodnota
 }
 """
-adress = open_load("adresy.geojson")
-for adr in adress ["features"]:
-    adress_list[adr["properties"]["@id"]] = {\
-        "housenumber": adr["properties"]["addr:housenumber"],\
-        "street": adr["properties"]["addr:street"],\
-        "coordinates": transfer_coor(adr["geometry"]["coordinates"][0], adr["geometry"]["coordinates"][1])}
-
-print(adress_list)
+#print(chosen)
+#print(adress_list)
 #print(adress_list["node/290026925"]["coordinates"])
 
 # jen pro ověření správného výběru kontejnerů
 '''for i in chosen:
     print(i["properties"]["PRISTUP"])'''
+print(f"počet adresních bodů: {counter_adr}")
+
