854df79af378807a73b22607c528086ac1bb2f08
Anna Kožíšková <koziskoa@natur.cuni.cz>
2021-12-18 22:05:05 +0100

    převedení částí kódů na fce

--- code.py
+++ code.py
@@ -1,98 +1,49 @@
-from math import sqrt
-from functions import open_load, transfer_coor
-
-# založení proměnných pro počet kontejnerů
-# založení slovníku pro zapsání veřejných kontejnerů
-counter = 0             # všechny kontejnery
-counter_public = 0      # kontejnery s veřejným přístupem
-chosen_cont = {}        # slovník pro veřejné kontejnery
-
-containers = open_load("kontejnery.geojson")
-#otevření a načtení souboru s kontejnery do proměnné containers
-for cont in containers ["features"]:
-# procházení proměnné containers ve "features"    
-    counter += 1
-    # při každém novém "features" přičte 1 do proměnné counter
-    # načítá počet VŠECH kontejnerů
-    if (cont ["properties"] ["PRISTUP"] == "volně"):
-    # pokud cyklus narazí v "properties na položku "PRISTUP", 
-    # ve které je hodnota "volně" -> zapíše vybrané vlastnosti: 
-    # ID, ulice, souřadnice daného kontejneru do slovníku chosen_cont
-        chosen_cont[cont["properties"]["ID"]] = {\
-        "Street name": cont["properties"]["STATIONNAME"],\
-        "Type": cont["properties"]["PRISTUP"],\
-        "coordinates": [float(cont["geometry"]["coordinates"][0]), 
-                        float(cont["geometry"]["coordinates"][1])
-                       ]} 
-        counter_public += 1
-        # načítá počet VEŘEJNÝCH kontejnerů
-# tisk počtu načtených kontejnerů    
-print(f"Načteno {counter} kontejnerů na tříděný odpad")
-print(f"        {counter_public} kontejnerů je veřejných\n")
-
-# založení proměnnéh pro počet adresních bodů
-# založení slovníku pro zapsání adres
-counter_adr = 0
-chosen_adr = {}
-
+from math import inf
+from functions import choose_address, choose_containers, distance, load_coor, open_load, MAX_LIMIT
+from statistics import median
+# načtení souborů a uložení do slovníků
+containers = open_load("kontejnery.geojson") 
 address = open_load("adresy.geojson")
-#otevření a načtení souboru s adresními body do proměnné address
-for adr in address ["features"]:
-# procházení proměnné address ve "features"
-# pro každou novou položku ve "features" uloží do slovníku chosen_adr 
-# vybrané vlastnosti adresy: ID, číslo domu, ulice, souřadnice 
-    chosen_adr[adr["properties"]["@id"]] = {\
-        "housenumber": adr["properties"]["addr:housenumber"],\
-        "street": adr["properties"]["addr:street"],\
-        "coordinates": transfer_coor(float(adr["geometry"]["coordinates"][0]), float(adr["geometry"]["coordinates"][1]))}
-    if adr["properties"]:
-        counter_adr += 1
-        # načítá počet adres
-#přehled jak vypadá strktura přepsaných adres
-"""
-"id":
-{
-    "housenumber": hodnota,
-    "street": hodnota,
-    "coordinates": hodnota
-}
-"""
-# tisk počtu adresních bodů
-print(f"počet adresních bodů: {counter_adr}\n")
+dict_containers = choose_containers(containers)
+dict_address = choose_address(address)
 
-# založení proměnných pro zjistění nejkratší vzdálenosti (min_dist),
-# nejdelší vzdálenosti (max_dist) a součet nejkratších vzdáleností(sum_dist)
-min_dist = 100000
-max_dist = 0
-sum_dist = 0
-for coor_adresa in chosen_adr:
+# počet kontejnerů a adresních bodů
+counter = len(containers["features"])
+counter_public = len(dict_containers)
+counter_adr = len(dict_address)
+
+# založení proměnných pro: 
+short_dist = float(inf)     #nejkratší vzdálenosti
+long_dist = 0               #nejdelší vzdálenosti
+sum_dist = 0                #součet nejkratších vzdáleností
+short_list = []             #seznam nejkratších vzdáleností
+for coor_adresa in dict_address:
     #procházení slovníku adres pro uložení polohy adresy
-    X=chosen_adr[coor_adresa]["coordinates"][0]
-    Y=chosen_adr[coor_adresa]["coordinates"][1]
-    for kont in chosen_cont:
-        #peocházení slovníku kontejnerů pro uložení polohy
-        X2=chosen_cont[kont]["coordinates"][0]
-        Y2=chosen_cont[kont]["coordinates"][1]
-        cal_dist = sqrt((X-X2)**2+(Y-Y2)**2)
-        # výpočet vzdálenosti mezi adresním bodem a kontejnerem
-        if cal_dist < min_dist:
-            # pokud je vypočtená vzdálenost menší než dosavadní minimální vzdálenost,
-            # přeuloží se aktuální vypočtená vzdálenost do proměnné min_dist,
-            # která reperezentuje nejkratší vzdálenost pro danou adresu ke kontejneru
-            min_dist = cal_dist
-    # poté, co se projde celý slovník kontejnerů
-    sum_dist += min_dist
-    # načítá nejmenší nalezenou vzdálenost z dané adresy ke kontejneru
+    adr_point = load_coor(dict_address[coor_adresa]["coordinates"])
+    for kont in dict_containers:
+        #procházení slovníku kontejnerů pro uložení polohy
+        cont_point = load_coor(dict_containers[kont]["coordinates"])
+        cal_dist = distance(adr_point,cont_point)
+        if cal_dist < short_dist:
+            short_dist = cal_dist     # reperezentuje nejkratší vzdálenost pro danou adresu ke kontejneru
 
-    if min_dist > max_dist:
+    sum_dist += short_dist
+    short_list.append(short_dist)
+    if short_dist > long_dist:
         # pokud je aktuální minimální vzdálenost větší než největší 
         # uložená maximální vzdálenost, uloží se do proměnné max_dist nová největší vzdálenost
-        max_dist = min_dist
-        # do proměnných níže se uloží jméno ulice a číslo domu
-        max_street = chosen_adr[coor_adresa]["street"]
-        max_hnumber = chosen_adr[coor_adresa]["housenumber"]
+        long_dist = short_dist
+        max_street = dict_address[coor_adresa]["street"]
+        max_hnumber = dict_address[coor_adresa]["housenumber"]
+    if short_dist > MAX_LIMIT:
+        print("Vzdálenost k nejbližšímu kontejneru je větší než 10 km")
+        quit()
 
-# v proměnné result se uloží průměrná vzdálenost k nejbližšímu kontejneru
-result = int(sum_dist/counter_adr)
+result = int(sum_dist/counter_adr)      # průměrná vzdálenost       
+med = median(short_list)
+print(f"Načteno {counter} kontejnerů na tříděný odpad")
+print(f"        {counter_public} kontejnerů je veřejných\n")
+print(f"Počet adresních bodů: {counter_adr}\n")
 print(f"Průměrná vzdálenost ke kontejneru je {result} m.")
-print(f"Nejdale ke kontejneru je z adresy {max_street} {max_hnumber} a to {max_dist:.0f} m.\n")
+print(f"Nejdale ke kontejneru je z adresy {max_street} {max_hnumber} a to {long_dist:.0f} m.")
+print(f"Medián vzdálenosti ke kontejnerům: {med:.0f} m.\n")
--- functions.py
+++ functions.py
@@ -1,28 +1,93 @@
 import json
 from pyproj import Transformer
+from math import sqrt
+from typing import Final
+from json.decoder import JSONDecodeError
+
+wgs2jtsk = Transformer.from_crs(4326,5514, always_xy=True)
 
 def open_load (name):
-# otevírá soubor typu json 
-# do parametru name se zadá název souboru včetně uvozovek např. "soubor.json" 
+    """# Otevírá soubor typu json 
+    - do parametru se zadá název souboru včetně uvozovek např. "soubor.json"
+    - vrací obsah souboru"""
     try:
         with open(name, encoding="utf-8") as geojsonfile:
             reader = json.load(geojsonfile)
         return reader
-    except ValueError:
-        print("Soubor je chybný.")
-        exit()
+    
     except FileNotFoundError:
-        print("Soubor se nepodařilo najít.")
+        print(f"Soubor {name} se nepodařilo najít.")
         exit()
     except PermissionError:
-        print("Soubor není programu přístupný.")
+        print(f"Soubor {name} není programu přístupný.")
+        exit()
+    except JSONDecodeError:
+        print(f"Soubor {name} je prázdný.")
         exit()
 
+def choose_containers (data_c):
+    """# Vytvoření slovníku kontejnerů
+    - pokud cyklus narazí v "properties na položku "PRISTUP", 
+    ve které je hodnota "volně" -> zapíše vybrané vlastnosti: 
+    ID, ulice, souřadnice daného kontejneru do slovníku"""
+    chosen_cont = {}        # slovník pro kontejnery
+    counter_free = 0
+    for cont in data_c ["features"]:
+        if len(cont["geometry"]["coordinates"]) != 2:
+            print(f"Kontejneru: {cont['properties']['ID']} chybí jedna nebo obě souřadnice. Kontejner přeskakuji.")
+            continue
+        if (cont ["properties"] ["PRISTUP"] == "volně"):
+            chosen_cont[cont["properties"]["ID"]] = {\
+            "street_name": cont["properties"]["STATIONNAME"],\
+            "type": cont["properties"]["PRISTUP"],\
+            "coordinates": [float(cont["geometry"]["coordinates"][0]), 
+                            float(cont["geometry"]["coordinates"][1])
+                        ]}
+            counter_free += 1
+    if counter_free == 0:
+        print("Soubor s kontejnery nemá ani jeden kontejner veřejný")
+        exit()
+    return chosen_cont
+
+def choose_address (data_a):
+    """# Vytvoření slovníku adres
+    - zapíše do slovníku vybrané vlastnosti adresy: ID, číslo domu, ulice, souřadnice
+    ## přehled jak vypadá strktura přepsaných adres
+    "id":
+    {
+        "housenumber": hodnota,
+        "street": hodnota,
+        "coordinates": hodnota
+    }
+    """
+    chosen_adr = {}         # založení slovníku pro adresy
+    for adr in data_a ["features"]: 
+        try:  
+            chosen_adr[adr["properties"]["@id"]] = {\
+                "housenumber": adr["properties"]["addr:housenumber"],\
+                "street": adr["properties"]["addr:street"],\
+                "coordinates": transfer_coor(float(adr["geometry"]["coordinates"][0]), float(adr["geometry"]["coordinates"][1]))}
+        except IndexError:
+            print(f"Adresnímu bodu: {adr['id']} chybí jedna nebo obě souřadnice. Adresu přeskakuji.")
+            continue
+    return chosen_adr
+
 def transfer_coor (x,y):
-# převod souřadnicového systému z WGS84 do JTSK
-# do paramteru x se uvede zeměpisná délka - např. 14.1564
-# do paramteru y se uveede zaměpisná šířka - např. 50.1481
-# funkce vrátí dvojici čísel x a y v souřadnicovém systému JTSK
-# např. x = -741687.450 a y = -1044981.860
-    wgs2jtsk = Transformer.from_crs(4326,5514, always_xy=True)
+    """# Převod souřadnicového systému z WGS84 do JTSK
+    - vrátí dvojici čísel x a y v souřadnicovém systému JTSK"""
     return wgs2jtsk.transform(x,y)
+        
+MAX_LIMIT: Final = 10000
+
+def load_coor (path, position0 = 0, position1 = 1):
+    """## Načtení souřadnic bodu
+    - vrátí dvojici čísel"""
+    x= path[position0]
+    y= path[position1]
+    return (x,y)
+
+def distance (point0, point1):
+    """# Výpočet vzdálenosti mezi 2 body
+    -do parametru vstupují proměnné jakožto dvojice čísel"""
+    dist = sqrt((point0[0]-point1[0])**2+(point0[1]-point1[1])**2)
+    return dist
